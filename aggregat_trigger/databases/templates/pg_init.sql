5296ccc9832e4236457ffba3438da465284083d3
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2014-12-05 17:26:05 +0100

    FIX 2 errors on templates

--- aggregat_trigger/databases/templates/pg_init.sql
+++ aggregat_trigger/databases/templates/pg_init.sql
@@ -1,11 +1,5 @@
 BEGIN;  
-
 LOCK TABLE {{table}} IN EXCLUSIVE MODE;
-
 TRUNCATE {{aggtable}};
-
-INSERT INTO {{aggtable}} (
-   SELECT {{column}}, count({{column}})
-   FROM {{table}};
-
+INSERT INTO {{aggtable}} (SELECT {{column}}, count({{column}}) FROM {{table}} GROUP BY {{column}});
 COMMIT;
