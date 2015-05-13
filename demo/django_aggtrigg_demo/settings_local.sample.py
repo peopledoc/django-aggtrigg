d1ed23df200cce8df5d52f7bb904b229793d2042
Bruno Bord <bruno.bord@novapost.fr>
2015-05-13 16:45:27 +0200

    refs #54 -- local settings example

---
+++ demo/django_aggtrigg_demo/settings_local.sample.py
@@ -0,0 +1,11 @@
+# This is a local settings example file.
+# Adapt this to fit your settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_aggtrig',
+        'USER': 'postgres',
+        'PASSWORD': 'pleaseputyournicepasswordhere',
+        'HOST': '127.0.0.1',
+    }
+}
